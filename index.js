f19e79532cf70dafbcb8acc02113bf33b404dbda
Grant Timmerman <granttimmerman@gmail.com>
2014-05-16 10:18:59 -0700

    Init

---
+++ index.js
@@ -0,0 +1,48 @@
+var http = require('http');
+
+var DOMAIN = 'http://xkcd.com/';
+var PATH = 'info.0.json';
+
+// Gets the current xkcd.
+// @param id [String|Number] The id of the xkcd. Blank for current xkcd.
+// @param cb [Function] The callback that passes (`err`, `data`)
+// @example current(2, function(err, data){ console.log(data); });
+function current (id, cb) {
+
+  var idURL = id ? id + '/' : '';
+  var url = DOMAIN + idURL + PATH;
+
+  http.get(url, function(res) {
+    var body = '';
+
+    res.on('data', function(chunk) {
+      body += chunk;
+    });
+
+    res.on('end', function() {
+      var data = JSON.parse(body);
+      cb(undefined, data);
+    });
+  }).on('error', function(err) {
+    cb(err);
+  });
+}
+
+// Gets the current xkcd. @see #current
+// If one parameter is passed, it gets the current xkcd
+// If two parameters are passed, [1]:id and [2]:callback
+// @example xkcd(1, function(err, data){});
+// @example xkcd(function(err, data){ console.log('latest'); });
+var xkcd = function () {
+  var id, cb;
+  if (arguments.length == 2) {
+    id = arguments[0];
+    cb = arguments[1];
+    current(id, cb);
+  } else {
+    cb = arguments[0];
+    current(undefined, cb);
+  }
+};
+
+module.exports = xkcd;
